d18ec163b9ff97a32683a66d90c617bb2de0ea54
Nicholas Humfrey <njh@aelius.com>
2015-01-03 23:12:30 +0000

    Switched from Gridster to Sortable by Lebedev Konstantin

--- public/js/marquette.js
+++ public/js/marquette.js
@@ -1,9 +1,6 @@
-var columns = 4;
-var rows = 4;
-var min_margin = 6;
 var textfill_config = {
     innerTag: 'span',
-    minFontPixels: 4,
+    minFontPixels: 6,
     maxFontPixels: 0
 };
 
@@ -20,82 +17,60 @@ $.postJSON = function(url, data, callback) {
 
 $(function(){ //DOM Ready
 
-    function serialise_params(tile, wgd)
-    {
-        var params = { row: wgd.row, col: wgd.col };
-        var blacklist = ['row', 'col', 'coords', 'sizex', 'sizey'];
-        for(var key in tile.data()) {
-            if (blacklist.indexOf(key) < 0) {
-                params[key] = tile.data(key);
-            }
-        }
-        return params;
-    }
-
-    function init_gridster()
-    {
-        var win = $( window );
-
-        var tile_size_x = Math.floor((win.width() - (columns * min_margin * 2) - min_margin) / columns);
-        if (tile_size_x % 2 == 1)
-            tile_size_x -= 1;
-
-        var height = win.height() - $('#footer').height();
-        var tile_size_y = Math.floor((height - (rows * min_margin * 2) - min_margin) / rows);
-        if (tile_size_y % 2 == 1)
-            tile_size_y -= 1;
-
-        var tile_size = Math.min(tile_size_x, tile_size_y);
-        var margin_x = Math.floor((win.width() - (tile_size * columns)) / (columns*2));
-        var margin_y = Math.floor((height - (tile_size * rows)) / (rows*2));
-
-        var gridster = $(".gridster").gridster({
-            widget_selector: ".tile",
-            widget_base_dimensions: [tile_size, tile_size],
-            widget_margins: [margin_x, margin_y],
-            serialize_params: serialise_params,
-            min_cols: columns,
-            max_cols: columns
-        }).data('gridster');
-
-        // Disable dragging
-        gridster.disable();
-
-        return gridster;
-    }
-
-    var gridster = init_gridster();
-
     $.getJSON("tiles",function(tiles) {
+        var container = $('#tiles');
         $.each(tiles, function(i, tile) {
-            var div = $("<div></div>").attr('id', "tile"+i).addClass('tile');
-            $.each(tile, function(k, v) { div.data(k, v) });
+            var item = $("<li></li>").attr('id', "tile"+i).addClass('tile');
+            $.each(tile, function(k, v) { item.data(k, v) });
 
             if (tile.type == 'button') {
                 var button = $('<button></button>').attr('type', 'button');
                 button.on("click", function(event) {
                     $.postJSON('/topics/'+tile.publish_topic, {payload: tile.publish_payload});
                 });
-                button.appendTo(div);
-                div.append("<label>"+tile.name+"</label>");
+                button.appendTo(item);
+                item.append("<label>"+tile.name+"</label>");
             } else if (tile.type == 'text') {
-                div.append('<div class="text"><span></span></div>');
-                div.append('<label>'+tile.name+'</label>');
+                item.append('<div class="text"><span></span></div>');
+                item.append('<label>'+tile.name+'</label>');
             } else {
-                div.html("Unknown tile type: "+tile.type);
+                item.html("Unknown tile type: "+tile.type);
             }
-
-            gridster.add_widget(div, size_x=1, size_y=1, tile.col, tile.row);
+            item.appendTo(container);
         });
-   });
+    });
 
-    function save_tiles(button) {
-        var tile_data = gridster.serialize();
+    function enableEditing(button) {
+        var container = $('#tiles');
+        container.sortable({
+          animation: 150, // ms, animation speed moving items when sorting, `0` — without animation
+          draggable: ".tile",
+          ghostClass: "ghost"
+        });
+
+        $('.tile').addClass('editable');
+        button.html('Save');
+    }
+
+    function serialiseTiles()
+    {
+        var tiles = [];
+        return tiles;
+    }
+
+    function saveTiles(button) {
+        var container = $('#tiles');
+        var tile_data = $('.tile').map(function() {
+            return $(this).data();
+        }).get();
+
+        console.log(tile_data);
+
         $.postJSON(
             'tiles', tile_data
         ).done(function(data, textStatus) {
-            $( '.tile' ).removeClass('editable');
-            gridster.disable();
+            $('.tile').removeClass('editable');
+            container.sortable('destroy');
             button.html('Edit...');
         });
     }
@@ -103,24 +78,12 @@ $(function(){ //DOM Ready
     $("#edit").on("click", function(event) {
         var button = $( event.target );
         if (button.html() == 'Edit...') {
-            $( '.tile' ).addClass('editable');
-            gridster.enable();
-            button.html('Save');
+            enableEditing(button);
         } else {
-            save_tiles(button);
+            saveTiles(button);
         }
     });
     
-
-    $( window ).resize(function() {
-        var gridster = $('.gridster').data('gridster');
-        if (gridster) {
-            gridster.destroy();
-        }
-        init_gridster();
-        $('.text').textfill(textfill_config);
-    });
-
     FastClick.attach(document.body);
 
     var source = new EventSource('/update-stream');
